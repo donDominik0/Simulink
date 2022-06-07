3a306e024a55acc1e20b4a6d4d92547cd0ea71d8
Dominik <dominik.macecevic@gmail.com>
2022-06-08 01:06:05 +0200

    seminar0

---
+++ seminar/seminar.m
@@ -0,0 +1,13 @@
+%% Parametri
+global Kv omegav ps pa B V0 qrm J b c Km
+Kv = 5.55e-7;
+omegav = 113;
+ps = 100e5;
+pa = 1e5;
+B = 1350e6;
+V0 = 150e-6;
+qrm = 25.6e-6;
+J = 0.00156;
+b = 0.5;
+c = 150;
+Km = 1;
